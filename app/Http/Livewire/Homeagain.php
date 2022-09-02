87a87c20dcb8d7012ca3ad6421f925bd336baa5d
HeartHacker99 <rashidmehmooda1@gmail.com>
2022-09-02 04:41:45 -0700

    Hotwire/turbo First

---
+++ app/Http/Livewire/Homeagain.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Homeagain extends Component
+{
+    public function render()
+    {
+        return view('livewire.homeagain');
+    }
+}
---
+++ resources/views/livewire/homeagain.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <h1>This is Home-Again</h1>
+
+    <a href="/home" style="color: red">Home</a>
+    <a href="/sweet" style="color: blue">Sweet</a>
+    <a href="/homeagain" style="color: green">Home-Again</a>
+</div>
